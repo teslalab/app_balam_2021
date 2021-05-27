2977cd523e57e648ad91ab8f1553142e68806779
Gabriel Monzon <gabriel.monzon39@gmail.com>
2021-05-27 00:14:00 -0600

    renamed app

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,22 @@ import 'package:stack_trace/stack_trace.dart';
 // This is added to route the logging info - which includes which file and where in the file
 // the message came from.
 //
-  void initLogger() {
-    Logger.root.level = Level.ALL;
-    Logger.root.onRecord.listen((LogRecord rec) {
-      final List<Frame> frames = Trace.current().frames;
-      try {
-        final Frame f = frames.skip(0).firstWhere((Frame f) =>
-            f.library.toLowerCase().contains(rec.loggerName.toLowerCase()) &&
-            f != frames.first);
-        print(
-            '${rec.level.name}: ${f.member} (${rec.loggerName}:${f.line}): ${rec.message}');
-      } catch (e) {
-        print(e.toString());
-      }
-    });
-  }
+void initLogger() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((LogRecord rec) {
+    final List<Frame> frames = Trace.current().frames;
+    try {
+      final Frame f = frames.skip(0).firstWhere((Frame f) =>
+          f.library.toLowerCase().contains(rec.loggerName.toLowerCase()) &&
+          f != frames.first);
+      print(
+          '${rec.level.name}: ${f.member} (${rec.loggerName}:${f.line}): ${rec.message}');
+    } catch (e) {
+      print(e.toString());
+    }
+  });
+}
+
 void main() {
   initLogger();
   runApp(MyApp());
@@ -31,11 +32,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'App Balam 2021',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MqttPage(title: 'Adafruit.io learning'),
+      home: MqttPage(title: 'App Balam 2021'),
     );
   }
 }
